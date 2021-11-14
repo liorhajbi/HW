ddf6135b71542c956b00a102d59d830456641c2e
lior hajbi <liorhajbi330@gmail.com>
2021-11-14 22:46:08 +0200

    initial

--- src/Q1.java
+++ src/Q1.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 public class Q1 {
     public static void main(String[]args){
-        
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("enter number");
+        int num =scanner.nextInt();
+      int value= Math.abs(num);
+       System.out.println("the return value is"+value);
+
+
     }
 
 }
